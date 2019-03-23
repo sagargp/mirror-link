da30cecfc97251166d2883d960194f90d3a3e892
Sagar Pandya <sagargp@gmail.com>
2019-03-22 21:29:59 -0700

    added client/server

--- client.py
+++ client.py
@@ -1,35 +1,16 @@
 import pyaudio
 import numpy as np
-import matplotlib.pyplot as plt
+import socket
+import struct
+import argparse
 
 
-def setup_plots():
-    f, ax = plt.subplots(2)
-
-    # Prepare the Plotting Environment with random starting values
-    x = np.arange(10000)
-    y = np.random.randn(10000)
-
-    # Plot 0 is for raw audio data
-    audio_plot, = ax[0].plot(x, y)
-    ax[0].set_xlim(0, 1000)
-    ax[0].set_ylim(-5000, 5000)
-    ax[0].set_title("Raw Audio Signal")
-    # Plot 1 is for the FFT of the audio
-    dfft_plot, = ax[1].plot(x, y)
-    ax[1].set_xlim(0, 5000)
-    ax[1].set_ylim(0, 80)
-    ax[1].set_title("Fast Fourier Transform")
-
-    # Show the plot, but without blocking updates
-    plt.tight_layout()
-    plt.subplots_adjust(hspace=0.3)
-    plt.pause(0.01)
-
-    return audio_plot, dfft_plot
+def send(ip, port, data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(data, (ip, int(port)))
 
 
-def main(chunk_size):
+def main(chunk_size, server, port):
     audio = pyaudio.PyAudio()
 
     # Claim the microphone
@@ -46,7 +27,7 @@ def main(chunk_size):
     print("| Press Ctrl+C to Break Recording |")
     print( "+---------------------------------+\n")
 
-    audio_plot, dfft_plot = setup_plots()
+    # audio_plot, dfft_plot = setup_plots()
 
     # Loop so program doesn't end while the stream is open
     running = True
@@ -59,18 +40,20 @@ def main(chunk_size):
 
             print(max(audio_data))
 
+            send(server, port, audio_data)
+
             # Fast Fourier Transform, 10*log10(abs) is to scale it to dB
             # and make sure it's not imaginary
             dfft = 10.*np.log10(abs(np.fft.rfft(audio_data)))
 
             # Force the new data into the plot, but without redrawing axes.
-            audio_plot.set_xdata(np.arange(len(audio_data)))
-            audio_plot.set_ydata(audio_data)
-            dfft_plot.set_xdata(np.arange(len(dfft))*10.)
-            dfft_plot.set_ydata(dfft)
+            # audio_plot.set_xdata(np.arange(len(audio_data)))
+            # audio_plot.set_ydata(audio_data)
+            # dfft_plot.set_xdata(np.arange(len(dfft))*10.)
+            # dfft_plot.set_ydata(dfft)
 
-            # Show the updated plot, but without blocking
-            plt.pause(0.01)
+            # # Show the updated plot, but without blocking
+            # plt.pause(0.01)
         except KeyboardInterrupt:
             running = False
 
@@ -81,4 +64,9 @@ def main(chunk_size):
 
 
 if __name__ == '__main__':
-    main(1024)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server', default='127.0.0.1')
+    parser.add_argument('port', default='9999')
+    args = parser.parse_args()
+
+    main(chunk_size=1024, server=args.server, port=args.port)
