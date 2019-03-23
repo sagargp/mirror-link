6bdefe5c82fe31208bebc3e2189d6222763f57aa
Sagar Pandya <sagargp@gmail.com>
2019-03-23 09:51:57 -0700

    Sending audio data over simple UDP stream

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ def send(ip, port, data):
     sock.sendto(data, (ip, int(port)))
 
 
-def main(chunk_size, server, port):
+def main(chunk_size, server, port, name):
     audio = pyaudio.PyAudio()
 
     # Claim the microphone
@@ -34,26 +34,11 @@ def main(chunk_size, server, port):
     while running:
         try:
             audio_data = stream.read(chunk_size, exception_on_overflow=False)
-
             # get and convert the data to float
-            audio_data = np.fromstring(audio_data, np.int16)
-
-            print(max(audio_data))
-
+            # audio_data = np.fromstring(audio_data, np.int16)
+            # print(max(audio_data))
+            data = struct.pack('10s2038s', name[0:10].encode('ascii'), audio_data)
             send(server, port, audio_data)
-
-            # Fast Fourier Transform, 10*log10(abs) is to scale it to dB
-            # and make sure it's not imaginary
-            dfft = 10.*np.log10(abs(np.fft.rfft(audio_data)))
-
-            # Force the new data into the plot, but without redrawing axes.
-            # audio_plot.set_xdata(np.arange(len(audio_data)))
-            # audio_plot.set_ydata(audio_data)
-            # dfft_plot.set_xdata(np.arange(len(dfft))*10.)
-            # dfft_plot.set_ydata(dfft)
-
-            # # Show the updated plot, but without blocking
-            # plt.pause(0.01)
         except KeyboardInterrupt:
             running = False
 
@@ -67,6 +52,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('server', default='127.0.0.1')
     parser.add_argument('port', default='9999')
+    parser.add_argument('name', default='bernie')
     args = parser.parse_args()
 
-    main(chunk_size=1024, server=args.server, port=args.port)
+    main(chunk_size=2048, server=args.server, port=args.port, name=args.name)
--- server.py
+++ server.py
@@ -1,10 +1,49 @@
+import struct
+import time
+import queue
+from threading import Thread
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import socket
 
 
-def setup_plots():
+class UDPServer(Thread):
+
+    def __init__(self, data_queue, ip, port, chunk_size, *args, **kwargs):
+        self._queue = data_queue
+        self._ip = ip
+        self._port = port
+        self._chunk_size = chunk_size
+        self._running = True
+
+        self.connected_clients = {}
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self._ip, int(self._port)))
+
+        while self._running:
+            data, addr = sock.recvfrom(self._chunk_size)
+            name, data = struct.unpack('10s2038s', data)
+            data = np.fromstring(data, np.int16)
+            name = name.decode('ascii')
+            self._queue.put(data)
+
+            if name not in self.connected_clients:
+                print('New client! {}'.format(name))
+            self.connected_clients[name] = time.time()
+
+
+def main(chunk_size, ip, port):
+    # create a data queue
+    q = queue.Queue()
+    # start up the visualization thread
+    server = UDPServer(q, ip, port, chunk_size)
+    server.start()
+
+    # first setup the plots
     f, ax = plt.subplots(2)
 
     # Prepare the Plotting Environment with random starting values
@@ -13,47 +52,33 @@ def setup_plots():
 
     # Plot 0 is for raw audio data
     audio_plot, = ax[0].plot(x, y)
-    ax[0].set_xlim(0, 1000)
+    ax[0].set_xlim(0, 1024)
     ax[0].set_ylim(-5000, 5000)
     ax[0].set_title("Raw Audio Signal")
-    # Plot 1 is for the FFT of the audio
-    dfft_plot, = ax[1].plot(x, y)
-    ax[1].set_xlim(0, 5000)
-    ax[1].set_ylim(0, 80)
-    ax[1].set_title("Fast Fourier Transform")
 
     # Show the plot, but without blocking updates
     plt.tight_layout()
     plt.subplots_adjust(hspace=0.3)
-    plt.pause(0.01)
 
-    return audio_plot, dfft_plot
-
-
-def main(chunk_size, ip, port):
-
-    audio_plot, dfft_plot = setup_plots()
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((ip, int(port)))
-
-    # Loop so program doesn't end while the stream is open
     running = True
     while running:
         try:
-            data, addr = sock.recvfrom(chunk_size)
-            data = np.fromstring(data, np.int16)
-
-            print(len(data))
-
+            data = q.get_nowait()
             # Force the new data into the plot, but without redrawing axes.
             audio_plot.set_xdata(np.arange(len(data)))
             audio_plot.set_ydata(data)
 
             # Show the updated plot, but without blocking
             plt.pause(0.001)
+            q.task_done()
         except KeyboardInterrupt:
             running = False
+            server._running = False
+        except queue.Empty:
+            pass
+
+    q.join()
+    server.join()
 
 
 if __name__ == '__main__':
@@ -62,4 +87,4 @@ if __name__ == '__main__':
     parser.add_argument('port', default='9999')
     args = parser.parse_args()
 
-    main(chunk_size=1024, ip=args.ip, port=args.port)
+    main(chunk_size=2048, ip=args.ip, port=args.port)
